3d92ae77f9f4e42b93dc739d3fba738e2be73d28
Anuragverma0002 <vermaanurag0209@gmail.com>
2025-09-04 11:45:37 +0530

    Update Vent.js

--- server/models/Vent.js
+++ server/models/Vent.js
@@ -4,15 +4,13 @@ const ventSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
-    required: false // Allow anonymous vents
+    required: true,
   },
   message: {
     type: String,
     required: true,
     trim: true,
-  }
-}, {
-  timestamps: true
-});
+  },
+}, { timestamps: true });
 
 module.exports = mongoose.model("Vent", ventSchema);
